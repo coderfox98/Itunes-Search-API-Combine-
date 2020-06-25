97a68462c3f3cb88bec073c2ac748d91ee8eec41
Rishabh Raj <rrishabh1808@gmail.com>
2020-06-25 15:07:21 +0530

    Initial Commit

---
+++ ItunesAPIAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ItunesAPIAssignment
+//
+//  Created by Rishabh Raj on 25/06/20.
+//  Copyright © 2020 rishstudios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
